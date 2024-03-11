6efc449356e3f07a3fa3939cd42656acfb2c9313
Kento Ooiwa <lunatic.heros@gmail.com>
2024-03-11 16:41:49 +0900

    first commit.

---
+++ FontGen/Program.cs
@@ -0,0 +1,29 @@
+﻿using SkiaSharp;
+
+var savePath = "tmp.png";
+
+var text = "あいうえおHello, world.1234１２３４";
+var color = SKColors.Black;
+var size = 100;
+var bg = SKColors.Transparent;
+
+var paint = new SKPaint();
+
+paint.Color = color;
+paint.TextSize = size;
+paint.Typeface = SKTypeface.FromFile("yojo.ttf");
+paint.SubpixelText = true;
+
+var drawSize = paint.MeasureText(text);
+
+using var surface = SKSurface.Create(new SKImageInfo((int)drawSize, (int)(paint.FontMetrics.Descent - paint.FontMetrics.Ascent) ));
+using var canvas = surface.Canvas;
+
+canvas.Clear(bg);
+canvas.DrawText(text,0, 0 - paint.FontMetrics.Ascent, paint);
+
+var img = surface.Snapshot();
+var tmp = img.Encode();
+
+using var fs = new FileStream(savePath,FileMode.CreateNew);
+tmp.SaveTo(fs);
